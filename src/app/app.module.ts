5f59d4f66eaa00d85659ae7b69f211e4d8bbc7f8
Homero Bonomini <homerobono@gmail.com>
2018-08-09 10:34:54 -0300

    CRUD User

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,58 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
+import { HttpClient, HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { AuthService } from './services/auth.service';
+import { AuthInterceptor } from './services/auth.interceptor';
+import { UsersComponent } from './users/users.component';
+import { UsersService } from './services/users.service';
+import { RegisterComponent } from './register/register.component';
+import { ResetComponent } from './reset/reset.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ProfileComponent } from './profile/profile.component';
+import { UserComponent } from './user/user.component';
+import { ErrorComponent } from './error/error.component';
+import { HomeComponent } from './home/home.component';
+import { ForgotComponent } from './forgot/forgot.component';
+import { NavButtonsComponent } from './nav-buttons/nav-buttons.component';
+import { RoutingModule } from './routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    UsersComponent,
+    RegisterComponent,
+    ResetComponent,
+    NavbarComponent,
+    ProfileComponent,
+    HomeComponent,
+    ErrorComponent,
+    ForgotComponent,
+    UserComponent,
+    NavButtonsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule, ReactiveFormsModule,
+    BrowserModule, BrowserAnimationsModule,
+    RoutingModule
   ],
-  providers: [],
+  providers: [
+    UsersService,
+    AuthService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true
+    },
+  ],
+  schemas: [ NO_ERRORS_SCHEMA ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
